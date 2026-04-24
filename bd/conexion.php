4a6d71b9a242362495c524a12ccf90cb0105d382
Yonnattal <46349201+Yonnattal@users.noreply.github.com>
2026-04-23 22:24:23 -0400

    Add files via upload

---
+++ bd/conexion.php
@@ -0,0 +1,19 @@
+
+
+<?php
+// Obtenemos los datos de forma segura desde el sistema
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
+// Conexión usando los datos de la nube
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+?>
