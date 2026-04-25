0df939a45c903396be04af6f2b4417c0fc74de14
Yonnattal <46349201+Yonnattal@users.noreply.github.com>
2026-04-24 22:45:56 -0400

    Update conexion.php

--- bd/conexion.php
+++ bd/conexion.php
@@ -2,6 +2,8 @@
 
 <?php
 // Obtenemos los datos de forma segura desde el sistema
+
+
 $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
@@ -9,7 +11,7 @@ $db   = getenv('DB_NAME');
 $port = getenv('DB_PORT');
 
 // Conexión usando los datos de la nube
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$conn = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'), getenv('DB_PORT'));
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
